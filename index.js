ac82372ac283f58dc0e62fa8dc95388ff757f78c
zxxzyyz <zxxzyyz2020@gmail.com>
2022-10-28 16:58:09 +0900

    Install body-Parser & nodemon. Test post handler

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-Parser");
+const { User } = require("./models/User");
 
+// Configuration for bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
 const mongoose = require("mongoose");
 mongoose
   .connect(
@@ -12,6 +19,14 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
